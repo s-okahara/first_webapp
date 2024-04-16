d5cf108bd62d1d8aa08e4ebee7b310fa3f8d5c5d
s-okahara <wafu4gen98@gmail.com>
2024-04-16 23:30:28 +0900

    やっとおわった。。。

---
+++ first_webapp/src/test/SetAppScopeServlet.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SetAppScopeServlet")
+public class SetAppScopeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public SetAppScopeServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getServletContext().setAttribute("app_name", "テストアプリケーション");
+
+        response.setContentType("text/html; charset=UTF-8");
+        var out = response.getWriter();
+        out.println("アプリケーションスコープに登録完了");
+    }
+}
